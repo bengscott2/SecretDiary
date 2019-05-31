1be358f0ef2f63331a9afcf2611e587a5b8fd7ea
Ben Scott <bengscott2@gmail.com>
2019-05-31 11:11:20 +0100

    added test for error with get_entries while locked and passing code

--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -2,7 +2,6 @@ require 'secret_diary'
 describe SecretDiary do
   it { is_expected.to respond_to(:lock) }
   it { is_expected.to respond_to(:unlock) }
-  it { is_expected.to respond_to(:get_entries) }
 
   it 'throws an error when trying to add_entry while locked' do
     secret_diary = SecretDiary.new
